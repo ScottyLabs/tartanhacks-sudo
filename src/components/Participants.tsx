3d0d34af4d365f594705123eaf4fc5546ee1cb70
Joyce Hong <joyce.hong8@gmail.com>
2023-04-01 00:15:01 -0400

    filtering tests

--- src/components/Participants.tsx
+++ src/components/Participants.tsx
@@ -11,55 +11,94 @@ import {
   Td,
   TableCaption,
   TableContainer,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
 } from '@chakra-ui/react'
+import { boolean } from 'zod';
+import { m } from 'framer-motion';
 
 function Participants() {
   const [data, setData] = useState<Participant[]>([]);
+  const [active, setActive] = useState<Participant[]>([]);
+  const [adminFilter, setAdminFilter] = useState<String>("");
+  const [statusFilter, setStatusFilter] = useState<String>("");
   const { data: session, status } = useSession();
 
   useEffect(() => {
     ParticipantsService.getParticipants("", session?.accessToken as string)
-      .then(data => setData(data))
+      .then(data => {setData(data); setActive(data)})
       .catch(error => console.error(error));
   }, []);
 
+  function randInt(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function filterAdmin() {
+    const options = ["", true, false]
+    const match = options[randInt(0, options.length)]
+    setAdminFilter(String(match))
+    setActive(data.filter(p => match ? p.admin == match : true))
+  }
+
+  function filterStatus() {
+    const options = ["", "CONFIRMED", "ADMITTED", "VERIFIED", "DECLINED"]
+    const match = options[randInt(0, options.length)]
+    setStatusFilter(match ?? "")
+    setActive(data.filter(p => match ? p.status == match : true))
+  }
+
   if (data) {
     return (
-      <TableContainer>
-        <Table variant='striped'>
-          <Thead>
-            <Tr>
-              <Th>id</Th>
-              <Th>email</Th>
-              <Th>admin</Th>
-              <Th>status</Th>
-              <Th>judge</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {data.map(item => {
-              return (
-                <tr key={item._id}>
-                  <td>{item._id}</td>
-                  <td>{item.email}</td>
-                  <td>{String(item.admin ?? false).toUpperCase()}</td>
-                  <td>{item.status}</td>
-                  <td>{String(item.judge ?? false).toUpperCase()}</td>
-                </tr>
-              );
-            })}
-          </Tbody>
-          <Tfoot>
-            <Tr>
-              <Th>id</Th>
-              <Th>email</Th>
-              <Th>admin</Th>
-              <Th>status</Th>
-              <Th>judge</Th>
-            </Tr>
-          </Tfoot>
-        </Table>
-      </TableContainer>
+      <div>
+        <div className="text-xl mb-8">PARTICIPANT DATA</div>
+        <TableContainer>
+          <Table size="sm" variant='striped'>
+            <Thead>
+              <Tr>
+                <Th>id</Th>
+                <Th>email</Th>
+                <Th className='hover:bg-yellow-100' onClick={filterAdmin}>
+                  {"admin" + (adminFilter && `: ${adminFilter}`)}
+                </Th>
+                <Th className='hover:bg-yellow-100' onClick={filterStatus}>
+                  {"status" + (statusFilter && `: ${statusFilter}`)}
+                </Th>
+                <Th className='hover:bg-yellow-100'>judge</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {active.map(participant => {
+                return (
+                  <Tr key={participant._id}>
+                    <Td>{participant._id}</Td>
+                    <Td>{participant.email}</Td>
+                    <Td>{String(participant.admin ?? false).toUpperCase()}</Td>
+                    <Td>{participant.status}</Td>
+                    <Td>{String(participant.judge ?? false).toUpperCase()}</Td>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+            <Tfoot>
+              <Tr>
+                <Th>id</Th>
+                <Th>email</Th>
+                <Th>admin</Th>
+                <Th>status</Th>
+                <Th>judge</Th>
+              </Tr>
+            </Tfoot>
+          </Table>
+        </TableContainer>
+      </div>
     );
   } else {
     return <p>Loading...</p>;
